9f45fbebb147b76aab7f7593a118cd5d59625c85
solitary-s <logic9711@gmail.com>
2020-05-24 21:39:24 +0800

    update .gitignore

--- itoken-service-redis/src/main/java/com/funtl/itoken/service/redis/RedisApplication.java
+++ itoken-service-redis/src/main/java/com/funtl/itoken/service/redis/RedisApplication.java
@@ -2,10 +2,9 @@ package com.funtl.itoken.service.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class RedisApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
